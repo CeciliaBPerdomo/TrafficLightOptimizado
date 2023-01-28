b1060ed692369fe118836b1014ae4b162ab00fe5
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2023-01-28 02:10:00 +0000

    Mejorado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
 import React from "react";
-
+import { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    const [change, setChange] = useState("");
+    const colores = [{
+        id : "red",
+        classStyle : "bg-danger"
+    },
+    {
+        id : "yellow",
+        classStyle : "bg-warning"
+    },
+    {
+        id : "green",
+        classStyle : "bg-success"
+    }
+]
+const alternar = () =>{
+    setInterval(()=>{
+
+    }, 1000)
+} 
+}
+    
+    return (
+        <div className="container m-5">
+            <div className="traffic mx-auto text-center bg-dark"></div>
+            <div className="main-box mx-auto text-center bg-dark rounded-5 p-1">
+            {
+                colores.map( ( {id,classStyle} ) => (
+                    <div 
+                        key={id} 
+                        className={`color ${classStyle} mt-2 ${(change === id ? "iluminado" : "")}`}  
+                        onClick={() => setChange(id)}>                        
+                    </div>
+                ))
+            }
+            </div>
+            <br />
+            <div className="d-flex justify-content-center">
+                <button className="btn btn-primary" onClick={alternar}>Alternar</button>
+            </div>
+        </div>
+    );
 
 export default Home;
